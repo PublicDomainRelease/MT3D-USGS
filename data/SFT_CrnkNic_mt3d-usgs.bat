a5ee778d6724c56e9c4b189165404c869564633b
USGS <gs-w_ogw_dev@usgs.gov>
2019-06-28 00:00:00 +1200

    MT3D-USGS Version 1.1.0 06/28/2019
    https://water.usgs.gov/water-resources/software/MT3D-USGS/mt3dusgs1.1.0.zip

--- data/SFT_CrnkNic_mt3d-usgs.bat
+++ data/SFT_CrnkNic_mt3d-usgs.bat
@@ -7,7 +7,7 @@ REM Generate FTL file using MODFLOW-NWT ver 1.1.4 (or greater)
 ..\..\bin\MF_NWT_1.1.4_64.exe CrnkNic_mf.nam
 
 REM Run MT3D-USGS only after FTL is generated
-..\..\bin\MT3D-USGS_64_1.0.1.exe  CrnkNic_mt.nam
+..\..\bin\MT3D-USGS_1.1.0_64.exe  CrnkNic_mt.nam
 
 ECHO.
 ECHO Run complete. Please press enter when you want to continue.
